22879cd00bf12280dc8c8865fa849ebf45a98967
Vulcanostrol <koen.brink.vd@gmail.com>
2016-01-21 18:50:05 +0100

    new package

---
+++ SpacePhysics/src/util/ScientificNumber.java
@@ -0,0 +1,16 @@
+package util;
+
+public class ScientificNumber {
+	
+	private float numberValue;
+	private int tensPower;
+	
+	public ScientificNumber(float value, int power) {
+		numberValue = value;
+		tensPower = power;
+	}
+	
+	public ScientificNumber multiply(ScientificNumber number) {
+		return null;
+	}
+}
