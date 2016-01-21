8ec2417002036f43f26389ed0d120a567aa6554b
Vulcanostrol <koen.brink.vd@gmail.com>
2016-01-21 18:09:20 +0100

    Update Main.java

--- SpacePhysics/src/main/Main.java
+++ SpacePhysics/src/main/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("This was changed!");
-		System.out.println("Another change!");
+		System.out.println("Does fetching work?");
 	}
 }
