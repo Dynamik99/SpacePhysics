ce17dc273e925a21ca023b68ebf9a7472b9df789
Vulcanostrol <koen.brink.vd@gmail.com>
2016-01-21 18:11:54 +0100

    commit

--- SpacePhysics/src/main/Main.java
+++ SpacePhysics/src/main/Main.java
@@ -3,8 +3,6 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("This was changed!");
-		System.out.println("Another change!");
+		System.out.println("PUSH!");
 	}
 }
