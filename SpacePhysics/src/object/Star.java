c07a8cfd04249437a8f8c44c0e0d4343612a67cc
Vulcanostrol <koen.brink.vd@gmail.com>
2016-01-24 15:15:42 +0100

    dghf

--- SpacePhysics/src/object/Star.java
+++ SpacePhysics/src/object/Star.java
@@ -1,6 +1,63 @@
 package object;
 
-public class Star {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Star extends Body {
 	
-	// STAR CODE HEYYY KOEN.
+	private int starRadius;
+	
+	private ArrayList<Body> bodies;
+	private ArrayList<Integer> distances;
+	private ArrayList<Float> velocities;
+	private ArrayList<Float> positions;
+	
+	public Star(int x, int y, int radius, long mass) {
+		super(x, y, mass);
+		starRadius = radius;
+		bodies = new ArrayList<Body>();
+		distances = new ArrayList<Integer>();
+		velocities = new ArrayList<Float>();
+		positions = new ArrayList<Float>();
+	}
+	
+	public void update() {
+		updateBodies();
+		move();
+	}
+	
+	public void updateBodies() {
+		for(int i = 0;i < bodies.size(); i++) {
+			float loop = (float) (2f * Math.PI * ((float) distances.get(i)));
+			float offset = positions.get(i);
+			offset += velocities.get(i) / loop;
+			while(offset > 1f) {
+				offset -= 1f;
+			}
+			while(offset < 0f) {
+				offset += 1f;
+			}
+			positions.set(i, offset);
+			float angle = (float) (offset * 2f * Math.PI);
+			bodies.get(i).setPosition((int) (Math.cos(angle) * distances.get(i)), (int) (Math.sin(angle) * distances.get(i)));
+		}
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.orange);
+		g.fillOval((int) getX() - starRadius, (int) getY() - starRadius, starRadius << 1, starRadius << 1);
+	}
+	
+	public void addBody(Body body, int distance, float velocity, float offset) {
+		bodies.add(body);
+		distances.add(distance);
+		velocities.add(velocity);
+		positions.add(offset);
+	}
+	
+	public ArrayList<Body> getBodies() {
+		return bodies;
+	}
 }
